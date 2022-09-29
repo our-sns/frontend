28c9f2933812ee8e59a3158b7571cdc8e2d703f9
songhuiyeon99 <a89919981z@gmail.com>
2022-09-29 13:26:08 +0900

    start project

---
+++ src/pages/PostListPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeaderContainer from '../containers/common/HeaderContainer';
+
+const PostListPage = () => {
+  return (
+    <>
+      <HeaderContainer />
+      <div>메인화면</div>
+    </>
+  );
+};
+
+export default PostListPage;
